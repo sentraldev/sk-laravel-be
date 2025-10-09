3632dd4ab05cf7ca059e568776596344b59ae8af
James Christian Wira <jamesch888@gmail.com>
2025-10-10 05:58:08 +0700

    add trust proxies

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TrustProxies
+{
+    protected $proxies = '*';
+    protected $headers = \Illuminate\Http\Request::HEADER_X_FORWARDED_ALL;
+
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        return $next($request);
+    }
+}
